1812c6360bc033bc51a06c0bb38c18af6fffe9f3
Shakim Harris <amenraprod@gmail.com>
2025-04-18 18:47:38 -0400

    redirect to auth page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { convexAuthNextjsMiddleware, createRouteMatcher, isAuthenticatedNextjs, nextjsMiddlewareRedirect  } from "@convex-dev/auth/nextjs/server";
 
-const isPublicPage = createRouteMatcher(['/signin']);
+const isPublicPage = createRouteMatcher(['/auth']);
  
 export default convexAuthNextjsMiddleware((request) => {
     if(!isPublicPage(request) && !isAuthenticatedNextjs()) { 
-        return nextjsMiddlewareRedirect(request, "/signin");
+        return nextjsMiddlewareRedirect(request, "/auth");
     }
 });
  
