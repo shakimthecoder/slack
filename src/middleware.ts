fbd8f3a9dedd536d9704772513c06649bdefe00a
Shakim Harris <amenraprod@gmail.com>
2025-04-18 05:13:03 -0400

    add middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { convexAuthNextjsMiddleware, createRouteMatcher, isAuthenticatedNextjs, nextjsMiddlewareRedirect  } from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(['/signin']);
+ 
+export default convexAuthNextjsMiddleware((request) => {
+    if(!isPublicPage(request)) { 
+        return nextjsMiddlewareRedirect(request, "/signin");
+
+    }
+});
+ 
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
