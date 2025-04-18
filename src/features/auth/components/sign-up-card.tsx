489de333dd1224f14587efd1d7dd744c8cfd67e8
Shakim Harris <amenraprod@gmail.com>
2025-04-18 01:47:34 -0400

    add title

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,14 +1,17 @@
-import { Card, CardHeader, CardDescription, CardContent } from '../../../components/ui/card';
+import { Card, CardHeader, CardDescription, CardContent, CardTitle } from '../../../components/ui/card';
 
 export const SignUpCard = () => {
     return (
        <Card className="w-full h-full p-8">
-        <CardHeader className="pt-0 px-0"> Sign up to continue. 
+        <CardHeader className="pt-0 px-0"> 
+        <CardTitle>
+            Sign up
+        </CardTitle>
         </CardHeader>
         <CardDescription> Use your email to sign up.</CardDescription>
         <CardContent className="space-y-5 px-0 pb-0">
             <form className="space-y-2.5">
-                
+
             </form>
 
         </CardContent>
