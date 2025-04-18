55860f540d5eeb5ccacab5cef63b0ac1967317d7
Shakim Harris <amenraprod@gmail.com>
2025-04-18 19:33:54 -0400

    add signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
+"use client";
+
+import { Button } from '../components/ui/button'
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
+  const { signOut } = useAuthActions();
   return (
    <div>
-    <AuthScreen />
+    Logged in!
+    <Button
+     onClick={() => signOut()}>
+      Sign Out
+     </Button>
     </div>
    
   );
