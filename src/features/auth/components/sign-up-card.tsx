b0a969e35ce3172e12acfdc30d212cf0dfb3b2c0
Shakim Harris <amenraprod@gmail.com>
2025-04-18 01:46:20 -0400

    card work

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,9 +1,17 @@
-import { Card } from '../../../components/ui/card';
+import { Card, CardHeader, CardDescription, CardContent } from '../../../components/ui/card';
 
 export const SignUpCard = () => {
     return (
        <Card className="w-full h-full p-8">
-        Sign Up
+        <CardHeader className="pt-0 px-0"> Sign up to continue. 
+        </CardHeader>
+        <CardDescription> Use your email to sign up.</CardDescription>
+        <CardContent className="space-y-5 px-0 pb-0">
+            <form className="space-y-2.5">
+                
+            </form>
+
+        </CardContent>
        </Card>
     )
 }
