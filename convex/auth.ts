01a1d3db7982e65c885c7f78b4fdc78f38c00bfa
Shakim Harris <amenraprod@gmail.com>
2025-04-18 19:37:00 -0400

    add google provider

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,6 @@
 import GitHub from "@auth/core/providers/github";
 import { convexAuth } from "@convex-dev/auth/server";
-
+import Google from "@auth/core/providers/google";
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
