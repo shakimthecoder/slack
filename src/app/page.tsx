a92fdd62f44418d4d8d158641c022bf535e842b3
Shakim Harris <amenraprod@gmail.com>
2025-04-19 15:29:21 -0400

    update screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import { Button } from '../components/ui/button'
+import  UserButton  from '../features/auth/components/user-button';
 import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
@@ -8,10 +7,7 @@ export default function Home() {
   return (
    <div>
     Logged in!
-    <Button
-     onClick={() => signOut()}>
-      Sign Out
-     </Button>
+    <UserButton />
     </div>
    
   );
