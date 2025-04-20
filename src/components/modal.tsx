22da4c193fb11b8583f4d977a08c38a3ff6b67c0
Shakim Harris <amenraprod@gmail.com>
2025-04-20 16:57:59 -0400

    add modal

---
+++ src/components/modal.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { CreateWorkspaceModal } from "@/features/webspaces/components/create-workspace-modal";
+
+export const Modals = () => {
+    return (
+        <>
+        <CreateWorkspaceModal />
+        </>
+    )
+}
