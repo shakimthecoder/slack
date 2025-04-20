07433ec9f917c2c19a6a9a4718ea195dd88b0adf
Shakim Harris <amenraprod@gmail.com>
2025-04-19 23:16:51 -0400

    add workspace

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,8 +1,14 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values"
  
 const schema = defineSchema({
   ...authTables,
+  workspaces: defineTable({
+     name: v.string(),
+     user: v.id("users"),
+     joinCode: v.string(),
+  })
   // Your other tables...
 });
  
