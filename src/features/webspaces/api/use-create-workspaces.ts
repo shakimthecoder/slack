5852ead12a40c73a253264978d8938ec3b78276b
Shakim Harris <amenraprod@gmail.com>
2025-04-20 18:07:23 -0400

    create mutation callback

---
+++ src/features/webspaces/api/use-create-workspaces.ts
@@ -0,0 +1,24 @@
+import { useMutation } from "convex/react"
+import { api } from '../../../../convex/_generated/api';
+import { useCallback } from "react";
+
+type Options = {
+    onSuccess?: () => void;
+    onError?: () => void;
+    onSettled?: () => void;
+}
+
+type RequestType = any;
+type ResponseType = any;
+
+export const useCreateWorkspace = () => {
+    const mutation = useMutation(api.workspaces.create);
+
+    const mutate = useCallback(async (values: RequestType, options: Options) => {
+        try {}
+        catch {
+
+        }
+        
+    }, [])
+}
