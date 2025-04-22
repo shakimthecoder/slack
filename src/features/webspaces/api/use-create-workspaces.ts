b39bd0ca84bc5a006dea9bb8b3d67659d045df9c
Shakim Harris <amenraprod@gmail.com>
2025-04-22 14:40:59 -0400

    add new states

--- src/features/webspaces/api/use-create-workspaces.ts
+++ src/features/webspaces/api/use-create-workspaces.ts
@@ -1,10 +1,10 @@
 import { useMutation } from "convex/react"
 import { api } from '../../../../convex/_generated/api';
 import { Doc, Id} from '../../../../convex/_generated/dataModel';
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
-type RequestType = { name: string }
-type ResponseType = Doc<"workspaces">;
+type RequestType = { name: string } | any;
+type ResponseType = Id<"workspaces"> | null;
 
 type Options = {
     onSuccess?: (data: Id<"workspaces">) => void;
@@ -16,9 +16,24 @@ type Options = {
 
 export const useCreateWorkspace = () => {
     const mutation = useMutation(api.workspaces.create);
+    const [data, setData] = useState<ResponseType>();
+    const [state, setState] = useState<("success" | "error" | "settled" | "pending" | null)>(null);
+    const [error, setError] = useState<Error | null>();
+    const [isPending, setIsPending] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isSettled, setIsSettled] = useState(false);
 
     const mutate = useCallback(async (values?: RequestType, options?: Options) => {
         try {
+            setData(null);
+            setError(null);
+            setIsError(false);
+            setIsSuccess(false);
+
+            setIsPending(true);
+           
+
             const response = await mutation(values);
             options?.onSuccess?.(response)
         }
@@ -29,11 +44,19 @@ export const useCreateWorkspace = () => {
             }
         }
         finally {
+            setIsSettled(true);
+            setIsPending(false);
             options?.onSettled?.()
         }
         
     }, [mutation])
      return {
         mutate,
+        isSettled,
+        isPending,
+        error,
+        isError,
+        data,
+        isSuccess,
      }
 }
